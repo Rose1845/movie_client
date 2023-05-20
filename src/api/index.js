dc569833500dc8d8980aae18d1fa8a403f7e5eef
Rose1845 <odhiamborose466@gmail.com>
2023-05-20 18:55:41 +0300

    integrate the movie-api

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,32 @@
 import axios from "axios";
 
-const BaseUrl="http://localhost:8080/api/v1/movies";
+// const BaseUrl="http://localhost:8080/api/v1";
 
-export default axios.create({
-    baseURL: BaseUrl,
+// export default publicAxios.create({
+//     baseURL: BaseUrl,
+    // headers: {
+    //     "Content-type": "application/json",
+    //     "Access-Control-Allow-Origin": "*",
+    //     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+
+    // }
+// });
+
+const BASE_URL="http://localhost:8080/api/v1";
+// export default axios.create({
+//     baseURL: BaseUrl,
+//     headers: {
+//         "Content-type": "application/json",
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+//     }
+// });
+export const  publicAxios = axios.create({
+    baseURL:BASE_URL,
     headers: {
-        "Content-type": "application/json"
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+
     }
-});
+  });
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import axios from '../api';
+import axios from 'axios';
+// import publicAxios from '../api/index';
+const BASE_URL="http://localhost:8080/api/v1";
+const  publicAxios = axios.create({
+    baseURL:BASE_URL,
+    headers: {
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+
+    }
+  });
+
 const Home=()=> {
 
     const [data, setData] = useState([]);
     const fetchMovies = async () => {
-        const response = await axios.get('');
+        // const response = await axios.get('/movies');
+        const response = await publicAxios.get('/movies');
         setData(response.data);
+        console.log(response.data);
     };
     useEffect(() => {
         fetchMovies();
@@ -15,13 +29,13 @@ const Home=()=> {
         <h1>
             {data.map((item) => (
             <div key={item.id}>
-                <h1>{item.title}</h1>
-                <p>{item.description}</p>
-                <img src={item.backdrops} alt="" />
+                <h1 style={{fontSize:'12px'}}>{item.title}</h1>
+                <p style={{fontSize:'8px'}}>{item.description}</p>
+                <img style={{width:'100px'}} src={item.backdrops[0]} alt="" />
             </div>
             ))}
         </h1>
         </div>
     );
-    }
-    export default Home;
+}
+export default Home;
